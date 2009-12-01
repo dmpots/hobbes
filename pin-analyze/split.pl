0bb6efd44e5c5baccd53c8d6ee63ace45ddc29cb
David M Peixotto <dmp@rice.edu>
2009-12-01 06:20:02 +0000

    Add analysis script

---
+++ pin-analyze/split.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+use File::Basename;
+
+foreach $file (@ARGV) {
+    open FH, '<', $file;
+    $mode = '';
+    header: while (<FH>) {
+        if(/^#.*\$(static).*/) {$mode= uc $1;last header;}
+    }
+    ($base, $dir, $ext) = fileparse($file, ".LOG");
+    $outf = $dir.$base.".".$mode.$ext;
+
+    open FH_STATIC, '>', $outf
+        or die "Unable to open file $outf: $!";
+    static: while (<FH>) {
+        if(/^#.*\$(dynamic).*/) {$mode= uc $1; last static;}
+        if(/^#.*/) {next;}
+        if(/^$/)   {next;}
+
+        ($opcode, $opname, $count) = split ' ', $_;
+        print FH_STATIC "($opcode, \"$opname\", $count)\n";
+    }
+    $outf = $dir.$base.".".$mode.$ext;
+    open FH_DYNAMIC, '>', $outf
+        or die "Unable to open file $outf: $!";
+    dynamic: while (<FH>) {
+        if(/^#.*/) {next;}
+        if(/^$/)   {next;}
+
+        ($opcode, $opname, $count) = split ' ', $_;
+        print FH_DYNAMIC "($opcode, \"$opname\", $count)\n";
+    }
+    
+    close FH; 
+    close FH_STATIC;
+    close FH_DYNAMIC;
+}
+
+
+
