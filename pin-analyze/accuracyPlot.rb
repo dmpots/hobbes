992d7e86273ea383a3a4fd0eb2d96c9bdcda26b5
David M Peixotto <dmp@rice.edu>
2010-01-19 18:15:41 +0000

    Added support for HSGP to accuracy plot

--- pin-analyze/accuracyPlot.rb
+++ pin-analyze/accuracyPlot.rb
@@ -35,7 +35,7 @@ if __FILE__ == $0 then
 files = {}
 ARGV.each do |file|
   ext = File.extname(file).gsub(/^\./, "")
-  if ext =~ /SPEC|HICC|HGCC|SHOOTOUT|SSGCC|HNHS/ then
+  if ext =~ /SPEC|HICC|HGCC|SHOOTOUT|SSGCC|HNHS|HSGP/ then
     pinTool =
     case file 
       when /jumpmix/   
