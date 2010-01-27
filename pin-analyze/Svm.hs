83664078718aba6764f8f29d61540ce6c2342a2a
David M Peixotto <dmp@rice.edu>
2010-01-27 17:21:40 +0000

    Changed Svm module to use external svm-predict instead of FFI

--- pin-analyze/Svm.hs
+++ pin-analyze/Svm.hs
@@ -1,13 +1,18 @@
 module Svm where
 
+import Control.Monad
 import Cluster
 import ClusterElement
 import Data.List
+--import Data.Datamining.Classification.LibSVM
 import Data.SVM
 import qualified Data.IntMap as IntMap
 import PinData
 import Numeric
 import System.IO
+import System.Exit
+import System.Process
+import Text.Printf
 
 type PredictionElement = (String, ProgramClass, Vector)
 type PredictedElement  = (String, ProgramClass, ProgramClass)
@@ -22,12 +27,21 @@ writeSVMFormattedData handle clusterData = mapM_ (printSVM handle) clusterData
     in
     hPutStr h classLabel >> hPutStrLn h points 
 
-
 writePredictionDataUsingModel::FilePath->Handle->[PinClusterElement]->IO ()
 writePredictionDataUsingModel modelFile h elements = do
-  model <- loadModel modelFile
-  let predicted = predictElements model elements
+  predictH <- openFile predictionFile WriteMode
+  writeSVMFormattedData predictH elements
+  hClose predictH
+  systemWithCheck cmd
+  results <- lines `liftM` readFile outFile
+  let predicted = zipWith zipF elements results
   hPutStrLn h (formatPredictions predicted) 
+  where
+  cmd = "./libsvm-2.9/svm-predict "++predictionFile++" "++modelFile++" "++outFile
+  predictionFile = "TRAIN.svm"
+  outFile = "PREDICT.out"
+  zipF element prediction = 
+    (shortName element, dataLabel element, toEnum (read prediction))
 
 formatPredictions :: [PredictedElement] -> String
 formatPredictions predictions = 
@@ -35,7 +49,7 @@ formatPredictions predictions =
   where
   (right, wrong) = partition correctPrediction predictions
   correctPrediction (_, a, b) = a == b
-  header =    "Accuracy = "++ formatAccuracy
+  header =    "Precision = "++ formatAccuracy
            ++ " ("++(show.length $ right)++"/"++(show.length $ predictions)++")"
            ++ "\n"
            ++ "RandIndx = "++formatRandIndex
@@ -50,10 +64,17 @@ formatPredictions predictions =
   formatPrediction (n,actual,predicted) =
      n ++ " [Acutal = "++(show actual)++", Predicted = "++(show predicted)++"]"
 
+writePredictionDataUsingFFI::FilePath->Handle->[PinClusterElement]->IO ()
+writePredictionDataUsingFFI modelFile h elements = do
+  model <- loadModel modelFile
+  let predicted = predictElements model elements
+  hPutStrLn h (formatPredictions predicted) 
+
 predictElements :: Model -> [PinClusterElement] -> [PredictedElement]
 predictElements model elements = predictIt converted
   where
   predictIt = map (\(name, klass, vec) -> (name, klass, predictVector model vec))
+  --predictIt = mapM (\(name, klass, vec) -> predictVector model vec >>= \p -> return (name, klass, p))
   converted = convertElementsToVectors elements
 
 convertElementsToVectors :: [PinClusterElement] -> [PredictionElement]
@@ -62,13 +83,16 @@ convertElementsToVectors = map convertElement
 convertElement :: PinClusterElement -> PredictionElement
 convertElement element =
   (shortName element, dataLabel element, IntMap.fromList vecList)
+  --(shortName element, dataLabel element, vecList)
   where
   vecList = map (\(x, y) -> (alEnum x, y)) (dataPoint element)
+  --vecList = map snd (dataPoint element)
 
 predictVector :: Model -> Vector -> ProgramClass
-predictVector model vector = prediction
+predictVector model vector = 
+  let prediction = predict model vector in
+  enumVal prediction
   where
-  prediction = enumVal $ predict model vector
   enumVal    = toEnum . fst . properFraction
   
 
@@ -79,3 +103,15 @@ svmRandIndex predicted = randIndex predicted partition1 partition2
   partition2 = actualClass
   predictedClass (_, _, p) = p
   actualClass    (_, a, _) = a
+
+
+systemWithCheck :: String -> IO ()
+systemWithCheck cmd 
+  = do
+      ec <- system cmd 
+      case ec of
+        ExitSuccess      -> return ()
+        ExitFailure code -> printf "execution failed (exit %d)\n" code >> 
+                            fail "Execute command failed"
+
+
