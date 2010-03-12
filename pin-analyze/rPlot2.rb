71c5e6de7d1aa4860292fba58f86bfc124904a74
David M Peixotto <dmp@rice.edu>
2010-03-12 22:54:35 +0000

    Move plot descriptions out of script into separate files

--- pin-analyze/rPlot2.rb
+++ pin-analyze/rPlot2.rb
@@ -67,124 +67,13 @@ tools= %w(opcodemix bblengthmix)
 ways= %w(dylib nodylib)
 cutoffs= %w(0 10)
 
-#
-# SPEC + nofib
-#
-tools.each do |tool|
-ways.each do |way|
-cutoffs.each do |cutoff|
-sep("SPEC + nofib #{tool.upcase} #{way} > #{cutoff}%")
-sets = [
-  "CProgram ../pin-run/RESULTS/LATEST/#{way}/spec.gcc.#{tool}",
-  "HaskellProgram ../pin-run/RESULTS/LATEST/#{way}/nofib.#{tool}"
-]
-mar(20)
-genDataAndScript(sets, "gshn_#{tool}", cutoff,
-  "x=\"right\",inset=c(-0.7, -0.7),cex=0.6,ncol=4", 
-  "Combined (SPEC + nofib) #{tool.capitalize} (> #{cutoff}% #{way})",
-  "% of total"
-)
+if __FILE__ == $0 then
+  ARGV.each do |plotFile|
+    puts plotFile
+    f = File.open(plotFile)
+    instance_eval(f.read)
+  end
 end
-end
-end
-
-#
-# SPEC + parallel
-#
-ways.each do |way|
-tools.each do |tool|
-cutoffs.each do |cutoff|
-sep("SPEC + parallel #{tool.upcase} #{way} > #{cutoff}%")
-sets = [
-  "CProgram ../pin-run/RESULTS/LATEST/#{way}/spec.gcc.#{tool}",
-  "HaskellProgram ../pin-run/RESULTS/LATEST/#{way}/parallel.ghc.#{tool}"
-]
-mar(20)
-genDataAndScript(sets, "gshp_#{tool}", cutoff,
-  "x=\"right\",inset=c(-0.7, -0.7),cex=0.6,ncol=4", 
-  "Combined (SPEC + parallel) #{tool.capitalize} (> #{cutoff}% #{way})",
-  "% of total"
-)
-end
-end
-end
-
-#
-# SPEC + parallel + shootout
-#
-ways.each do |way|
-tools.each do |tool|
-cutoffs.each do |cutoff|
-sep("SPEC + parallel + shootout #{tool.upcase} #{way} > #{cutoff}%")
-sets = [
-  "CProgram ../pin-run/RESULTS/LATEST/#{way}/spec.gcc.#{tool}",
-  "ParallelGhc ../pin-run/RESULTS/LATEST/#{way}/parallel.ghc.#{tool}",
-  "ShootoutGhc ../pin-run/RESULTS/LATEST/#{way}/shootout.ghc.#{tool}"
-]
-mar(20)
-genDataAndScript(sets, "gshpht_#{tool}", cutoff,
-  "x=\"right\",inset=c(-0.7, -0.7),cex=0.6,ncol=4", 
-  "Combined (SPEC + parallel + shootout) #{tool.capitalize} (> #{cutoff}% #{way})",
-  "% of total"
-)
-end
-end
-end
-
-#
-# nofib only
-#
-ways.each do |way|
-tools.each do |tool|
-cutoffs.each do |cutoff|
-sep("nofib only #{tool.upcase} #{way} > #{cutoff}%")
-sets = [
-  "HaskellProgram ../pin-run/RESULTS/LATEST/#{way}/nofib.#{tool}"
-]
-if cutoff == "0" then
-mar(20)
-labelOpts = "x=\"right\",inset=c(-0.7, -0.7),cex=0.8,ncol=3"
-else
-mar(10)
-labelOpts = "x=\"right\",inset=c(-0.2, -0.2),cex=0.9"
-end
-
-genDataAndScript(sets, "hn_#{tool}", cutoff,
-  labelOpts,
-  "Nofib #{tool.capitalize} (> #{cutoff}% #{way})",
-  "% of total"
-)
-end
-end
-end
-
-#
-# SPEC only
-#
-ways.each do |way|
-tools.each do |tool|
-cutoffs.each do |cutoff|
-sep("SPEC only #{tool.upcase} #{way} > #{cutoff}%")
-sets = [
-  "CProgram ../pin-run/RESULTS/LATEST/#{way}/spec.gcc.#{tool}"
-]
-if tools == "opcodemix" then
-  mar(20)
-  labelOpts="x=\"right\",inset=c(-0.7, -0.7),cex=0.6,ncol=4"
-else
-  mar(10)
-  labelOpts="x=\"right\",inset=c(-0.2, -0.2),cex=0.6,ncol=4" 
-end
-
-genDataAndScript(sets, "gs_#{tool}", cutoff,
-  labelOpts,
-  "SPEC #{tool.capitalize} (> #{cutoff}% #{way})",
-  "% of total"
-)
-end
-end
-end
-
 
 #
 # END
