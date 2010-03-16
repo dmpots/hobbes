c273e3ac5719832d74ef481f80764dcb2ac9a679
David M Peixotto <dmp@rice.edu>
2010-03-16 15:51:59 +0000

    Added script to install results files in place for running SVM

---
+++ pin-run/RESULTS/install.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/ruby
+require 'pp'
+require 'FileUtils'
+
+$cleanOnly = false
+if(ARGV.grep(/--clean/).count > 0) then
+  $cleanOnly = true
+end
+$exts = %w(opcodemix bblengthmix)
+$extsRE = $exts.join("|")
+
+def expandDir(d) 
+  $exts.map{|ext| Dir["#{d}/*.#{ext}"]}.flatten
+end
+
+# Clean old files
+files = expandDir(".")
+if files.count > 0 then
+  puts "Found current files: "
+  pp files
+  print "Really delete these directories? (Y/N) "
+  r = $stdin.gets
+  if r =~ /y|yes/i then
+    puts "Deleting files"
+    FileUtils.rm_rf(files)
+  end
+end
+
+# Copy new files
+srcDirs = ARGV.reject{|a| a =~ /--clean/}
+if $cleanOnly || srcDirs.empty? then
+  exit 0
+end
+
+srcDirs = srcDirs.map {|d| 
+  if d =~ /\.#{$extsRE}$/ then
+    d
+  else
+    expandDir(d)
+  end
+}.flatten
+
+puts "Copying files from #{srcDirs.pretty_inspect}"
+FileUtils.cp_r(srcDirs, ".")
+
+
+
+
