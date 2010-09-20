002861c422533af4c61a652ff0cc6ee5ef1a93f3
David M Peixotto <dmp@rice.edu>
2010-09-20 14:58:26 -0500

    Updated analysis script to create pdfs

--- ANALYSIS/R/plot.R
+++ ANALYSIS/R/plot.R
@@ -1,21 +1,11 @@
 ######################################################
 # DATA
 ######################################################
-#x <- read.table("nofib.dat", header=TRUE, row.names=1) 
-#datatitle <- "Nofib"
-x <- read.table("fibon.dat", header=TRUE, row.names=1) 
-datatitle <- "Fibon"
 
-
-######################################################
-# FUNCTIONS
-######################################################
-ellipse <- function(x0, a, y0, b) {
-  theta <- seq(0, 2 * pi, length=100)
-  xs <- x0 + a * cos(theta)
-  ys <- y0 + b * sin(theta)
-  lines(xs, ys, col="red")
-}
+qH <- 4.75
+qW <- 6.25
+qF <- "Times"
+qT <- "pdf"
 
 #label points
 #library(calibrate)
@@ -27,51 +17,65 @@ ellipse <- function(x0, a, y0, b) {
 # GRAPHS
 ######################################################
 
+doPlots <- function(x, datatitle){
 #
 # ICache vs. Correlation
 #
+fName     <- paste(datatitle, "-Icache-correlation-full", ".pdf", sep="")
+quartz(height=qH, width=qW, family=qF, type=qT, file=fName)
 plot(x$BR_MISSP_EXEC/x$L1I_MISSES, 
      x$CYCLES_L1I_MEM_STALLED/x$UNHALTED_CORE_CYCLES, 
      xlab="Correlation (Branch Mispredictions / I-Cache Misses)", 
      ylab="I-Cache Stall% (of Total Execution Time)")
+q3 <- summary(x$BR_MISSP_EXEC/x$L1I_MISSES)[5]
+abline(v=q3, col="red")
+text(q3, 0.4, "Q3", pos=4)
 title(paste(datatitle, "I-Cache Stall% vs. Correlation"))
+dev.off()
 
-ellipse(10, 20, 0.4, 0.4)
-
+fName     <- paste(datatitle, "-Icache-correlation-zoom", ".pdf", sep="")
+quartz(height=qH, width=qW, family=qF, type=qT, file=fName)
 plot(x$BR_MISSP_EXEC/x$L1I_MISSES, 
      x$CYCLES_L1I_MEM_STALLED/x$UNHALTED_CORE_CYCLES, 
      xlab="Correlation (Branch Mispredictions / I-Cache Misses)", 
      ylab="I-Cache Stall% (of Total Execution Time)",
-     xlim=c(0,10))
-title(paste(datatitle, "I-Cache Stall% vs. Correlation -- Zoomed"))
-
+     xlim=c(0,q3))
+title(paste(datatitle, "I-Cache Stall% vs. Correlation -- Upto Q3"))
+dev.off()
 
 #
 # ICache vs. TotalCycles
 #
+fName     <- paste(datatitle, "-Icache-cycles-full", ".pdf", sep="")
+quartz(height=qH, width=qW, family=qF, type=qT, file=fName)
 plot(x$UNHALTED_CORE_CYCLES,
      x$CYCLES_L1I_MEM_STALLED/x$UNHALTED_CORE_CYCLES, 
      xlab="Total Cycles", 
      ylab="I-Cache Stall% (of Total Execution Time)")
 title(paste(datatitle, "I-Cache Stall% vs. Total Cycles"))
+q3 <- summary(x$UNHALTED_CORE_CYCLES)[5]
+abline(v=q3, col="red")
+text(q3, 0.4, "Q3", pos=4)
+dev.off()
 
-ellipse(3e8, 5e8, 0.4, 0.4)
 
+fName     <- paste(datatitle, "-Icache-cycles-zoom", ".pdf", sep="")
+quartz(height=qH, width=qW, family=qF, type=qT, file=fName)
 plot(x$UNHALTED_CORE_CYCLES,
      x$CYCLES_L1I_MEM_STALLED/x$UNHALTED_CORE_CYCLES, 
      xlab="Total Cycles", 
      ylab="I-Cache Stall% (of Total Execution Time)",
-     xlim=c(0,1e9))
-title(paste(datatitle, "I-Cache Stall% vs. Total Cycles -- Zoomed"))
+     xlim=c(0,summary(x$UNHALTED_CORE_CYCLES)[5]))
+title(paste(datatitle, "I-Cache Stall% vs. Total Cycles -- Upto Q3"))
+dev.off()
+}
 
-# hpg == 40
-# maillist == 51
 
-plot(x$UNHALTED_CORE_CYCLES,
-     x$BR_MISSP_EXEC/x$UNHALTED_CORE_CYCLES,
-     xlab="Total Cycles", 
-     ylab="Number of Branch Mispredictions",
-     xlim=c(0,5e8))
-title(paste(datatitle, "Branch Mispredictions vs. Total Cycles"))
-
+######################################################
+# MAIN
+######################################################
+nofib <- read.table("nofib.dat", header=TRUE, row.names=1)
+doPlots(nofib, "Nofib")
+fibon <- read.table("fibon.dat", header=TRUE, row.names=1)
+doPlots(fibon, "Fibon")
 
